53530c2619b32038493bfb0329ce1ff827643cf7
exaudi24si-max <exaudi24si@mahasiswa.pcr.ac.id>
2025-10-21 09:40:42 +0700

    mengelompokkan view p7

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PegawaiController;
-use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
+use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\PelangganController;
-
-
+use App\Http\Controllers\QuestionController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,13 +17,13 @@ Route::get('/pcr', function () {
 });
 Route::get('/mahasiswa', function () {
     return 'Halo Mahasiswa';
-}) ->name('mahasiswa.show');
+})->name('mahasiswa.show');
 
 Route::get('/nama/{param1}', function ($param1) {
-    return 'Nama saya: '.$param1;
+    return 'Nama saya: ' . $param1;
 });
 Route::get('/nim/{param1?}', function ($param1 = '') {
-    return 'NIM saya: '.$param1;
+    return 'NIM saya: ' . $param1;
 });
 Route::get('mahasiswa/{param1}', [MahasiswaController::class, 'show']);
 
@@ -34,21 +32,13 @@ Route::get('mahasiswa/{param1}', [MahasiswaController::class, 'show']);
 Route::get('mahasiswa/{param1}', [MahasiswaController::class, 'show']);
 
 Route::get('/home', [HomeController::class, 'index'])
-->name('home');
+    ->name('home');
 Route::get('/pegawai', [PegawaiController::class, 'index']);
 
 Route::post('question/store', [QuestionController::class, 'store'])
-		->name('question.store');
-
+    ->name('question.store');
 
 Route::get('dashboard', [DashboardController::class, 'index'])
     ->name('dashboard');
 
-
-    Route::resource('pelanggan', PelangganController::class);
-
-
-
-
-
-
+Route::resource('pelanggan', PelangganController::class);
